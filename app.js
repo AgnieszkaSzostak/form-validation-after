e36e46c742b0db5a9d1caabd13c59d8105eb4084
Agnieszka Szostak <agnieszkaszostak94@gmail.com>
2022-09-21 12:37:35 +0200

    Add DOM elements - form

--- app.js
+++ app.js
@@ -2,125 +2,137 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    const formEl = document.querySelector('form');
-    const ulEl = document.querySelector('ul');
-
+    
+    
+    const fields = [
+        {
+            name: 'firstName',
+            label: 'Imię',
+            required: true,
+            pattern: '^[a-zA-Z –-]+$',
+        },
+        {
+            name: 'lastName',
+            label: 'Nazwisko',
+            required: true,
+            pattern: '^[a-zA-Z –-]+$',
+        },
+        { name: 'street', label: 'Ulica', required: true },
+        {
+            name: 'houseNumber',
+            label: 'Numer budynku',
+            type: 'number',
+            required: true,
+        },
+        { name: 'flatNumber', label: 'Numer mieszkania', type: 'number' },
+        {
+            name: 'zip',
+            label: 'Kod pocztowy',
+            pattern: '^[0-9]{2}-[0-9]{3}$',
+            required: true,
+        },
+        {
+            name: 'city',
+            label: 'Miasto',
+            required: true,
+            pattern: '^[a-zA-Z –-]+$',
+        },
+        { name: 'voivodeship', label: 'Województwo', required: true },
+        {
+            name: 'mobileNumber',
+            label: 'Numer telefonu komórkowego',
+            type: 'number',
+            pattern: '^[1-9]{9}$',
+            required: true,
+        },
+    ];
     // sprawdzam, czy formularz został wyszukany i dopiero przypisuję nasłuchiwanie zdarzenia submit
-    if (formEl) {
-        formEl.addEventListener('submit', handleSubmit);
-    }
+    // ZADANIE DODATKOWE: wygeneruj formularz HTML na podstawie kodu tablicy obiektów: fields
+    const form = document.createElement('form');
+    form.setAttribute('action', '');
+    form.setAttribute('method', 'post');
+    form.setAttribute('novalidate', '');
+    
+ 
+    fields.forEach(function (field) {
+        const {name, label, required = false, type = 'text', pattern = null} = field;
 
+        const labelEl = document.createElement('label');
+        labelEl.innerText = label;
+        // utwórz element <input>
+        // dodaj do <input> atrybuty z odpowiednimi wartościami (reszta właściwości obiektu)
+        const inputEl = document.createElement('input');
+        inputEl.setAttribute('name', name);
+        inputEl.setAttribute('required', required);
+        inputEl.setAttribute('type', type);
+        inputEl.setAttribute('pattern', pattern);
+        labelEl.appendChild(inputEl);
+        const divEl = document.createElement('div');
+        divEl.appendChild(labelEl);
+        form.appendChild(divEl);
+    });
+    
+    const submitButton = document.createElement('button');
+    const divEl = document.createElement('div');
+    submitButton.setAttribute('type', 'submit');
+    submitButton.innerText = 'Wyślij';
+    divEl.appendChild(submitButton);
+    form.appendChild(divEl);
+    const body = document.querySelector('body');
+    const script = document.querySelector('script');
+    body.insertBefore(form, script);
+    const uList = document.createElement('ul');
+    body.insertBefore(uList, form);
+    form.addEventListener('submit', handleSubmit);
+    
     function handleSubmit(e) {
-        // blokuję automatyczne wysłanie formularza
         e.preventDefault();
-
+        
         const errors = [];
-
-        const fields = [
-            {
-                name: 'firstName',
-                label: 'Imię',
-                required: true,
-                pattern: '^[a-zA-Z –-]+$',
-            },
-            {
-                name: 'lastName',
-                label: 'Nazwisko',
-                required: true,
-                pattern: '^[a-zA-Z –-]+$',
-            },
-            { name: 'street', label: 'Ulica', required: true },
-            {
-                name: 'houseNumber',
-                label: 'Numer budynku',
-                type: 'number',
-                required: true,
-            },
-            { name: 'flatNumber', label: 'Numer mieszkania', type: 'number' },
-            {
-                name: 'zip',
-                label: 'Kod pocztowy',
-                pattern: '^[0-9]{2}-[0-9]{3}$',
-                required: true,
-            },
-            {
-                name: 'city',
-                label: 'Miasto',
-                required: true,
-                pattern: '^[a-zA-Z –-]+$',
-            },
-            { name: 'voivodeship', label: 'Województwo', required: true },
-            // {
-            //     name: 'mobileNumber',
-            //     label: 'Numer telefonu komórkowego',
-            //     type: 'number',
-            //     pattern: '^[1-9]{9}$',
-            //     required: true,
-            // },
-        ];
-
         fields.forEach(function (field) {
-            // wykorzystuję właściwość obiektu o nazwie name, by pobrać wartość konkretnego elementu formularza
-            const value = formEl.elements[field.name].value;
+            const {name, label, required = false, type = 'text', pattern = null} = field;
 
-            if (field.required) {
+            const value = form.elements[name].value;
+
+            if (required) {
                 if (value.length === 0) {
-                    errors.push('Dane w polu ' + field.label + ' są wymagane.');
+                    errors.push('Dane w polu ' + label + ' są wymagane.');
                 }
             }
 
-            if (field.type === 'number') {
+            if (type === 'number') {
                 if (Number.isNaN(Number(value))) {
                     errors.push(
-                        'Dane w polu ' + field.label + ' muszą być liczbą.'
+                        'Dane w polu ' + label + ' muszą być liczbą.'
                     );
                 }
             }
 
-            if (field.pattern) {
-                const reg = new RegExp(field.pattern);
+            if (pattern) {
+                const reg = new RegExp(pattern);
                 if (!reg.test(value)) {
                     errors.push(
                         'Dane w polu ' +
-                            field.label +
+                            label +
                             ' zawierają niedozwolone znaki lub nie są zgodne z przyjętym w Polsce wzorem.'
                     );
                 }
             }
         });
 
-        // czyszczę listę błędów
-        ulEl.innerHTML = '';
+        uList.innerHTML = '';
         if (errors.length === 0) {
             alert('Dane zostały wypełnione prawidłowo!');
-
-            // czyszczę pola po prawidłowym wypełnieniu formularza
             fields.forEach(function (el) {
-                formEl[el.name].value = '';
+                form[el.name].value = '';
             });
         } else {
             errors.forEach(function (text) {
                 const liEl = document.createElement('li');
                 liEl.innerText = text;
-
-                ulEl.appendChild(liEl);
+                uList.appendChild(liEl);
             });
         }
     }
 }
 
-// ZADANIE DODATKOWE: wygeneruj formularz HTML na podstawie kodu tablicy obiektów: fields
-
-// utwórz element <form>
-// ustaw na nim nasłuchiwanie na zdarzenie submit i dodaj funkcję, która to zdarzenie obsłuży (możesz wykorzystać naszą funkcję handleSubmit)
-fields.forEach(function (field) {
-    // Dla każdego elementu tablicy (field):
-    // utwórz element <label>
-    // dodaj do <label> innerText z właściwości name
-    // utwórz element <input>
-    // dodaj do <input> atrybuty z odpowiednimi wartościami (reszta właściwości obiektu)
-    // dodaj <input> do jego rodzica: <label>
-    // nieobowiązkowo: stwórz <div> i wstaw tam <label> jako dziecko
-    // dodaj <div> lub <label> (zależy, na co się zdecydowałeś) do rodzica: <form>
-});
-// dodaj element <form> do drzewa DOM
